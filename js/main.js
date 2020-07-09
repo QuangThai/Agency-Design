45aece44c293f09a178298a703f1f9cc17fd4ef9
QuangThai <nguyenquangthaib1@gmail.com>
2020-07-09 17:11:53 +0700

    change responsive testimonial

--- js/main.js
+++ js/main.js
@@ -44,28 +44,28 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
 
-let slideIndex = 1;
-showSlides(slideIndex);
+// let slideIndex = 1;
+// showSlides(slideIndex);
 
-function plusSlides(n) {
-   showSlides(slideIndex += n);
-}
+// function plusSlides(n) {
+//    showSlides(slideIndex += n);
+// }
 
-function currentSlide(n) {
-   showSlides(slideIndex = n);
-}
-function showSlides(n) {
-   let i;
-   let slides = document.getElementsByClassName("testimonial__image");
-   let dots = document.getElementsByClassName("testimonial__dots-one");
-   if (n > slides.length) { slideIndex = 1 }
-   if (n < 1) { slideIndex = slides.length }
-   for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-   }
-   for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" testimonial__dots-one--active", "");
-   }
-   slides[slideIndex - 1].style.display = "block";
-   dots[slideIndex - 1].className += " testimonial__dots-one--active";
-}
+// function currentSlide(n) {
+//    showSlides(slideIndex = n);
+// }
+// function showSlides(n) {
+//    let i;
+//    let slides = document.getElementsByClassName("testimonial__image");
+//    let dots = document.getElementsByClassName("testimonial__dots-one");
+//    if (n > slides.length) { slideIndex = 1 }
+//    if (n < 1) { slideIndex = slides.length }
+//    for (i = 0; i < slides.length; i++) {
+//       slides[i].style.display = "none";
+//    }
+//    for (i = 0; i < dots.length; i++) {
+//       dots[i].className = dots[i].className.replace(" testimonial__dots-one--active", "");
+//    }
+//    slides[slideIndex - 1].style.display = "block";
+//    dots[slideIndex - 1].className += " testimonial__dots-one--active";
+// }
